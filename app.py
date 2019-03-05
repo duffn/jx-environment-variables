d9d0bed10de51e46193c8c3864afbcf4d3dc9b92
Nicholas Duffy <duffn@outlook.com>
2019-03-05 14:23:32 -0700

    Add branch based variables (#1)
    * Add branch based variables
    
    * Add missing end tag in template
    
    * Indent branchName properly

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import os
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT_NUMBER = 8080
@@ -9,10 +11,11 @@ class MyHandler(BaseHTTPRequestHandler):
   def do_GET(self):
     """Handler for GET requests"""
     self.send_response(200)
-    self.send_header('Content-type','image/png')
+    self.send_header('Content-type','text/plain')
     self.end_headers()
-    with open('logo.png', 'rb') as f:
-      self.wfile.write(f.read())
+
+    branch_name = os.environ.get('BRANCH_NAME', 'NOBRANCH')
+    self.wfile.write(branch_name.encode())
 
 try:
   server = HTTPServer(('', PORT_NUMBER), MyHandler)
